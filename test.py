0b43d333ce6cd9a222a0de44bfa288839e354843
Ernest Gottschalk <Ayrnee@Ernests-MacBook-Pro.local>
2017-09-29 16:05:26 -0400

    Initial python code

---
+++ test.py
@@ -0,0 +1,33 @@
+'''
+Interactive language with build tools and package managers
+package managers-anaconda and pip
+
+
+'''
+
+import numpy
+
+
+
+def main():
+    x = 5
+    print x
+
+    x = "hello"
+    print x
+
+    lissy = []
+    lissy.append(51)
+    print lissy
+    lissy.append("boi")
+    print lissy.pop()
+    print lissy
+
+    d = {"fourteen":14, "seven":7}
+    #in Java HashMap<string,int> = new HashMap<string,int>()
+    print d["fourteen"]
+    print
+
+
+if __name__ == '__main__':
+    main()
