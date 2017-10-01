09a0a036add57f886ec1ab991602794e848bdb7e
Ernest Gottschalk <ayrnee@gmail.com>
2017-10-01 18:47:01 -0400

    now with more loops

--- test.py
+++ test.py
@@ -3,6 +3,25 @@ Interactive language with build tools and package managers
 package managers-anaconda and pip
 
 size of any iterable comes with function len() for length
+
+loops:
+range function takes in one mandatory arg and two default params
+range(3): returns a list => [0,1,2]
+range(number of what you want, starting num,step)
+
+for i in range(5):
+    print i
+    #prints 0-4
+
+lissy = ["helo", "boi", 7]
+for i in lissy:
+    print i
+    #prints the list
+
+for i in range(len(lissy)):
+    print lissy[i]
+
+
 '''
 
 import numpy
@@ -10,24 +29,26 @@ import numpy
 
 
 def main():
-    x = int(raw_input("give me a number "))
+    # x = int(raw_input("give me a number "))
+    #
+    # x = 5
+    # print x
+    #
+    # x = "hello"
+    # print x
+    #
+    # lissy = []
+    # lissy.append(51)
+    # print lissy
+    # lissy.append("boi")
+    # print lissy.pop()
+    # print lissy
+    #
+    # d = {"fourteen":14, "seven":7}
+    # #in Java HashMap<string,int> = new HashMap<string,int>()
+    # print d["fourteen"]
 
-    x = 5
-    print x
 
-    x = "hello"
-    print x
-
-    lissy = []
-    lissy.append(51)
-    print lissy
-    lissy.append("boi")
-    print lissy.pop()
-    print lissy
-
-    d = {"fourteen":14, "seven":7}
-    #in Java HashMap<string,int> = new HashMap<string,int>()
-    print d["fourteen"]
 
 
 
