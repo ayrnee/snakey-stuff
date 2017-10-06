5a169cc89a6922d6b5f9c2ed2093e663a00e68a3
Mohammad Afshar <ma2510@nyu.edu>
2017-10-06 13:52:12 -0400

    added sample class with basic constructor

---
+++ classes_prep.py
@@ -0,0 +1,20 @@
+'''
+this is called the docstring - it's the highlevel program overview
+'''
+
+import numpy as np # we import the module numpy as the keyword np
+
+class Burger():
+    def __init__():
+        ## this is the constructor, this makes the class for us
+        ## everytime we instatiate the class, this function gets called
+        ## every declaration requires initialization
+        self.tomato = False
+        self.bun = "fire"
+        self.patty = "lamb"
+
+def main():
+    array = np.ndarray([1, 2, 3, 4])
+    # instead of:
+    array = numpy.ndarray([1, 2, 3, 4])
+    
